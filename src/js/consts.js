aa2c7206cd7b61bcdd5d2176cf0b9a2ba16ecec6
Jerry-Jefferson <karzovsnastassia@gmail.com>
2023-07-14 19:08:07 +0300

    Add more words

--- src/js/consts.js
+++ src/js/consts.js
@@ -1,2 +1,2 @@
-export const WORDS = ["code", "programming", "car", "tulip"];
+export const WORDS = ["code", "programming", "car", "tulip", "phone", "hair", "development", "wedding", "universe", "modernism"];
 export const KEYBOARD_LETTERS = Array.from("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
